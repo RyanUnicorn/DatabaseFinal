6d2671a3a5e4740aa9dd2cef3672d44e1d75a7d0
RyanUnicorn <ryan112345ryan@gmail.com>
2023-01-07 22:42:04 +0800

    modify the ApiRouter.js to reduce code duplication

--- Routers/ApiRouter.js
+++ Routers/ApiRouter.js
@@ -3,20 +3,22 @@ const auth = require('../Middlewares/Auth');
 const controller = require('../Controllers/ApiController');
 const router = express.Router();
 
-router.post('/Login',express.urlencoded({extended: false}) , controller.postLogin);
 router.get('/Logout', auth(), controller.getLogout);
 
-router.post('/Admin/AddBuilding', auth('Admin'), express.urlencoded({extended: false}) , controller.postAdminAddBuilding);
-router.post('/Admin/AddRoom', auth('Admin'), express.urlencoded({extended: false}) , controller.postAdminAddRoom);
-router.post('/Admin/AssignStudent', auth('Admin'), express.urlencoded({extended: false}) , controller.postAdminAssignStudent);
-router.post('/Admin/UpdateEquipment', auth('Admin'), express.urlencoded({extended: false}) , controller.postAdminUpdateEquipment);
-router.post('/Admin/postAdminUpdateApplication', auth('Admin'), express.urlencoded({extended: false}) , controller.postAdminUpdateApplication);
-router.post('/Admin/DeleteViolation', auth('Admin'), express.urlencoded({extended: false}) , controller.postAdminDeleteViolation);
-router.post('/DormAdmin/DeleteViolation', auth('DormAdmin'), express.urlencoded({extended: false}) , controller.postDormAdminDeleteViolation);
-router.post('/DormAdmin/NewViolation', auth('DormAdmin'), express.urlencoded({extended: false}) , controller.postDormAdminNewViolation);
-router.post('/Admin/DeleteAnnouncement', auth('Admin'), express.urlencoded({extended: false}) , controller.postAdminDeleteAnnouncement);
-router.post('/DormAdmin/DeleteAnnouncement', auth('DormAdmin'), express.urlencoded({extended: false}) , controller.postDormAdminDeleteAnnouncement);
-router.post('/PostAnnouncement', auth('Admin DormAdmin'), express.urlencoded({extended: false}) , controller.postAnnouncement);
+router.use(express.urlencoded({extended: false}));
+
+router.post('/Login', controller.postLogin);
+router.post('/Admin/AddBuilding', auth('Admin'), controller.postAdminAddBuilding);
+router.post('/Admin/AddRoom', auth('Admin'), controller.postAdminAddRoom);
+router.post('/Admin/AssignStudent', auth('Admin'), controller.postAdminAssignStudent);
+router.post('/Admin/UpdateEquipment', auth('Admin'), controller.postAdminUpdateEquipment);
+router.post('/Admin/postAdminUpdateApplication', auth('Admin'), controller.postAdminUpdateApplication);
+router.post('/Admin/DeleteViolation', auth('Admin'), controller.postAdminDeleteViolation);
+router.post('/DormAdmin/DeleteViolation', auth('DormAdmin'), controller.postDormAdminDeleteViolation);
+router.post('/DormAdmin/NewViolation', auth('DormAdmin'), controller.postDormAdminNewViolation);
+router.post('/Admin/DeleteAnnouncement', auth('Admin'), controller.postAdminDeleteAnnouncement);
+router.post('/DormAdmin/DeleteAnnouncement', auth('DormAdmin'), controller.postDormAdminDeleteAnnouncement);
+router.post('/PostAnnouncement', auth('Admin DormAdmin'), controller.postAnnouncement);
 
 
 module.exports = router;
